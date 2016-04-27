ab1c57988c6a664a42316579e387170b1b309df3
polelf <polelf@tom.com>
2016-04-28 01:15:26 +0800

    modify build bat

--- build_x64.bat
+++ build_x64.bat
@@ -1,14 +1,20 @@
-set BOOST_ROOT=d:\workspace\project\local\Boost\include\boost-1_55
-set Qt5_DIR=d:\Qt\Qt5.5.1_x64\5.5\msvc2013_64
-set Qt5Core_DIR=d:\Qt\Qt5.5.1_x64\5.5\msvc2013_64\lib\cmake
-set Qt5Widgets_DIR=d:\Qt\Qt5.5.1_x64\5.5\msvc2013_64\lib\cmake
-set IDA_DIR=d:\tools\IDA6.8
-set IDA_SDK_DIR=d:\tools\IDA6.8\sdk\idasdk68
-set X64DBG_SDK_DIR=d:\workspace\research\x64dbg\release\pluginsdk
+@echo off
+
+if exist usrenv_x64.bat (call usrenv_x64.bat)
+
+if "%BOOST_ROOT%"=="" set BOOST_ROOT=d:\workspace\project\local\Boost\include\boost-1_55
+if "%Qt5_DIR%"=="" set Qt5_DIR=d:\Qt\Qt5.5.1_x64\5.5\msvc2013_64
+if "%Qt5Core_DIR%"=="" set Qt5Core_DIR=d:\Qt\Qt5.5.1_x64\5.5\msvc2013_64\lib\cmake
+if "%Qt5Widgets_DIR%"=="" set Qt5Widgets_DIR=d:\Qt\Qt5.5.1_x64\5.5\msvc2013_64\lib\cmake
+if "%IDA_DIR%"=="" set IDA_DIR=d:\tools\IDA6.8
+if "%IDA_SDK_DIR%"=="" set IDA_SDK_DIR=d:\tools\IDA6.8\sdk\idasdk68
+if "%X64DBG_SDK_DIR%"=="" set X64DBG_SDK_DIR=d:\workspace\research\Debugger\x64dbg\release\pluginsdk
 
 mkdir build64
 cd build64
 
 cmake -G "Visual Studio 12 2013 Win64" -D IDA_64_BIT_EA_T=1 ../src && cmake-gui ../src
+
+cd ..
 
 pause
--- build_x86.bat
+++ build_x86.bat
@@ -1,12 +1,14 @@
 @echo off
 
-set BOOST_ROOT=d:\workspace\project\local\Boost\include\boost-1_55
-set Qt5_DIR=d:\Qt\Qt5.5.1_x86\5.5\msvc2013
-set Qt5Core_DIR=d:\Qt\Qt5.5.1_x86\5.5\msvc2013\lib\cmake
-set Qt5Widgets_DIR=d:\Qt\Qt5.5.1_x86\5.5\msvc2013\lib\cmake
-set IDA_DIR=d:\tools\IDA6.8
-set IDA_SDK_DIR=d:\tools\IDA6.8\sdk\idasdk68
-set X64DBG_SDK_DIR=d:\workspace\research\x64dbg\release\pluginsdk
+if exist usrenv.bat (call usrenv.bat)
+
+if "%BOOST_ROOT%"=="" set BOOST_ROOT=d:\workspace\project\local\Boost\include\boost-1_55
+if "%Qt5_DIR%"=="" set Qt5_DIR=d:\Qt\Qt5.6.0\5.6\msvc2013
+if "%Qt5Core_DIR%"=="" set Qt5Core_DIR=d:\Qt\Qt5.6.0\5.6\msvc2013\lib\cmake
+if "%Qt5Widgets_DIR%"=="" set Qt5Widgets_DIR=d:\Qt\Qt5.6.0\5.6\msvc2013\lib\cmake
+if "%IDA_DIR%"=="" set IDA_DIR=d:\tools\IDA6.8
+if "%IDA_SDK_DIR%"=="" set IDA_SDK_DIR=d:\tools\IDA6.8\sdk\idasdk68
+if "%X64DBG_SDK_DIR%"=="" set X64DBG_SDK_DIR=d:\workspace\research\Debugger\x64dbg\release\pluginsdk
 
 mkdir build32
 cd build32
